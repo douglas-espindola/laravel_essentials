04a542445aefc523905e1ba9486f08f93f19b59c
Douglas Espindola <douglas.spindola15@gmail.com>
2023-01-21 19:00:55 -0300

    criado migrate

--- database/migrations/2023_01_19_002244_create_posts_table.php
+++ database/migrations/2023_01_19_002244_create_posts_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('body')->nullable();
+            $table->string('image')->nullable();
             $table->boolean('is_published')->default(0);
             $table->timestamps();
         });
