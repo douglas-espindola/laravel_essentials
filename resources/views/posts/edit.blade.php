683bc27790455ec98c5c3da82674c6fc74feaf3d
Douglas Espindola <douglas.spindola15@gmail.com>
2023-01-24 21:33:01 -0300

    recuperando value do select no edit

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -37,6 +37,14 @@
 					<span class="text-red-400 text-sm">{{ $message }}</span>
 				@enderror
 			</div>
+            <div class="m-2 p-2">
+                <label for="tags" class="block mb-2 text-sm font-medium text-gray-900">Select an option</label>
+                <select id="tags" name="tags[]" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" multiple>
+                    @foreach ($tags as $tag)
+                        <option value="{{ $tag->id }}" @selected($post->tags->contains($tag->id))>{{ $tag->name }}</option>
+                    @endforeach
+                </select>
+            </div>
             <div>
                 <label for="">Published</label>
                 <input type="checkbox" name="is_published" value="is_published" @checked(old('is_published', $post->is_published)) />
