c3b08ab4c9e34a226182f25123fd3b9334d4ed60
Douglas Espindola <douglas.spindola15@gmail.com>
2023-01-25 23:15:03 -0300

    criado envio de email

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TagController;
+use App\Mail\TestMail;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +24,10 @@ Route::get('/', function () {
 
 Route::resource('/posts', PostController::class)->middleware(['auth']);
 Route::resource('/tags', TagController::class)->middleware(['auth']);
+
+Route::get('/contact', function () {
+    Mail::to('test@email.com')->send(new TestMail());
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
